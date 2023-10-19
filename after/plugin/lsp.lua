d622d9763f2075745db5aeaaeecef136ed47d9d9
Gage Hilyard <gagehilyard@gmail.com>
2023-10-18 20:52:15 -0600

    last commit before changing to lazy

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,7 +20,6 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -47,32 +46,32 @@ lsp.set_preferences({
 })
 
 
-local ls = require('luasnip')
-lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+--  local ls = require('luasnip')
+--  lsp.on_attach(function(client, bufnr)
+    --  local opts = {buffer = bufnr, remap = false}
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    --  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    --  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    --  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    --  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    --  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    --  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    --  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    --  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    --  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    --  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
-    vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-    vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-    vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+    --  vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+    --  vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+    --  vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
 
-    vim.keymap.set({"i", "s"}, "<C-E>", function()
-        if ls.choice_active() then
-            ls.change_choice(1)
-        end
-    end, {silent = true})
+    --  vim.keymap.set({"i", "s"}, "<C-E>", function()
+        --  if ls.choice_active() then
+            --  ls.change_choice(1)
+        --  end
+    --  end, {silent = true})
 
-end)
+--  end)
 
 
 lsp.set_sign_icons({
